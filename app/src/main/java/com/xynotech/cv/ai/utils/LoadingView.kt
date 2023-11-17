74d10621b8e1fda80eae7ede35b798ee820da1de
Ghulam Dastageer <ghulam.dastageer@xynotech.com>
2023-11-17 19:57:06 +0500

    loading view

---
+++ app/src/main/java/com/xynotech/cv/ai/utils/LoadingView.kt
@@ -0,0 +1,48 @@
+package com.xynotech.cv.ai.utils
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+
+
+@Preview
+@Composable
+fun previe() {
+
+    Column(
+        Modifier
+            .fillMaxSize()
+            .background(color = Color.White),
+        horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center) {
+
+        LoadingView(modifier = Modifier, text = "Processing Check")
+    }
+
+}
+
+@Composable
+fun LoadingView(modifier:Modifier,text:String) {
+    Card(
+        Modifier
+            .fillMaxWidth(0.75f), colors = CardDefaults.cardColors(containerColor = Color(0xFFF8D1FF))) {
+        Column(modifier, horizontalAlignment = Alignment.CenterHorizontally) {
+
+            LoadingAnimation3()
+
+            Text(text = text, textAlign = TextAlign.Center, fontSize = 16.sp, fontFamily = font(), fontWeight = FontWeight.SemiBold)
+        }
+    }
+}
